8f12e84e32df9b873d4f298ba779d748d76cea6e
LouisXVI07 <anablaptop@gmail.com>
2026-01-20 17:12:17 +0530

    ff

--- jan 2026/2191D1.cpp
+++ jan 2026/2191D1.cpp
@@ -2,11 +2,55 @@
 using namespace std;
 
 void solve(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    
+    int st=0;
+    int fg=1;
+    int cl=0,op=0;
+    
+    for (int i=0;i<n;i++){
+        if (fg){
+            if (s[i]=='('){
+                
+                st++;
+            }
+            else {
+                fg=0;
+                while(i<n){
+                    if (s[i]==')')cl++;
+                    if (s[i]=='('){
+                        st++;
+                        
+                        break;
+                    }
+                    
+                    i++;
+                }
+                cl--;
+
+            }
+        }
+       
+        else {
+            if (s[i]=='(')op++;
+            else cl++;
+        } 
+    }
+    if (st>cl){
+        cout<<-1<<'\n';
+        return;}
+    
+    int dif =cl-st;
+    cout<<2*st+ 2*min(dif , op)<<'\n';
 
 }
+
 int main(){
     int t=1;
-    //cin>>t;
+    cin>>t;
     while (t--){
         solve ();
     }
