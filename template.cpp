ad0cef4d7964d388324ef1046b59c37a084edd10
LouisXVI07 <anablaptop@gmail.com>
2026-01-22 14:29:25 +0530

    ff

--- template.cpp
+++ template.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-
+//long long->int
 void solve(){
 
 }
 int32_t main(){
     int t=1;
-    //cin>>t;
+    cin>>t;
     while (t--){
         solve ();
     }
