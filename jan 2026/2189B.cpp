6ba88873ceb6e9ac5fd72e4db67ae4e637460b10
LouisXVI07 <anablaptop@gmail.com>
2026-01-25 15:09:08 +0530

    f

---
+++ jan 2026/2189B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+//long long->int
+void solve(){
+
+}
+int32_t main(){
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve ();
+    }
+}
